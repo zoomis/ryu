15a682d2215f7ee2f189a7a5fbe52e7131016a59
Isaku Yamahata <yamahata@valinux.co.jp>
2012-03-23 19:19:11 +0900

    app/wsapi: make WSPathComponent inherit object
    Later we create subclass of WSPathComponent so that make super() work.
    app/wsapi: super().__init__()
    
    Signed-off-by: Isaku Yamahata <yamahata@valinux.co.jp>

--- ryu/app/wsapi.py
+++ ryu/app/wsapi.py
@@ -358,7 +358,7 @@ class WSPathExtractResult:
         self.error = error
 
 
-class WSPathComponent:
+class WSPathComponent(object):
     """Base class for WS path component extractors"""
 
     def __init__(self):
@@ -366,7 +366,7 @@ class WSPathComponent:
 
         Currently this does nothing but that may change in the future.
         Subclasses should call this to be sure."""
-        pass
+        super(WSPathComponent, self).__init__()
 
     def __str__(self):
         """Get the string representation of the path component
